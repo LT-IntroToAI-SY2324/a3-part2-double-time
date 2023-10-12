b1a5cd822fc89e467a705a6f33fcd33f3e9426b0
Ofigueroa8 <143109566+Ofigueroa8@users.noreply.github.com>
2023-10-12 09:59:26 -0500

    Add files via upload

---
+++ colleges.py
@@ -0,0 +1,43 @@
+from typing import List, Tuple
+
+college_db: List[Tuple[str, List[str], int, str, List[str]]] = [
+    (
+        "depaul university", #school
+        [
+            "biology",
+            "biochemistry",
+            "acconting",
+            "finance",
+        ], # majors
+        20917, # students
+        "chicago, illinois", # location
+        [
+            "basketball",
+            "cross country",
+            "soccer",
+            "tennis",
+            "track & field",
+        ], # sports
+
+    ),
+    (
+        "loyola university chicago", 
+        [
+            "business",
+            "marketing",
+            "social science",
+            "psychology",
+        ],
+        11703,
+        "chicago, illinois", 
+        [
+            "basketball",
+            "cross country",
+            "soccer",
+            "tennis",
+            "track & field",
+            "volleyball",
+        ], 
+
+    ),
+]
