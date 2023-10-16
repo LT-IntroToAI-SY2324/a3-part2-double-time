158ee5c4617d4b6f71267f4f6262a7cf70c07d96
Your Name <you@example.com>
2023-10-16 10:08:56 -0500

    added get functions

--- coding.py
+++ coding.py
@@ -1,1 +1,18 @@
+from colleges import colleges_db
+from match import match
+from typing import List, Tuple, Callable, Any
 
+def get_school(c: List[Tuple[str, List[str], int, str, List[str]]]) -> str:
+    return c[0]
+
+def get_majors(c: List[Tuple[str, List[str], int, str, List[str]]]) -> List[str]:
+    return c[1]
+
+def get_students(c: List[Tuple[str, List[str], int, str, List[str]]]) -> int:
+    return c[2]
+
+def get_location(c: List[Tuple[str, List[str], int, str, List[str]]]) -> str:
+    return c[3]
+
+def get_sports(c: List[Tuple[str, List[str], int, str, List[str]]]) -> List[str]:
+    return c[4]
