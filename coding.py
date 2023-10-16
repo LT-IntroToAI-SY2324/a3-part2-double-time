0a4155e6a80af397fed110851191a19082c3227c
Omar Figueroa <ofigueroa8@cps.edu>
2023-10-16 10:21:29 -0500

    All functions commit

--- coding.py
+++ coding.py
@@ -1,4 +1,4 @@
-from colleges import colleges_db
+from colleges import college_db
 from match import match
 from typing import List, Tuple, Callable, Any
 
@@ -18,4 +18,51 @@ def get_sports(c: List[Tuple[str, List[str], int, str, List[str]]]) -> List[str]
     return c[4]
 
 
+def school_by_major(m: List[str]) -> List[str]:
+    result = []
 
+    for c in college_db:
+        major = get_majors(c)
+        for a in major:
+            if a == m[0]:
+                result.append(get_school(c))
+
+    return result
+
+def schools_greater(m: List[str]) -> List[str]:
+    result = []
+
+    for c in college_db:
+        if get_students(c) >= int(m[0]):
+            result.append(get_school(c))
+
+    return result
+
+def schools_less(m: List[str]) -> List[str]:
+    result = []
+
+    for c in college_db:
+        if get_students(c) < int(m[0]):
+            result.append(get_school(c))
+
+    return result
+
+def school_by_location(m: List[str]) -> List[str]:
+    result = []
+
+    for c in college_db:
+        if get_location(c) == m[0]:
+            result.append(get_school(c))
+
+    return result
+
+def school_by_sport(m: List[str]) -> List[str]:
+    result = []
+    
+    for c in college_db:
+        sport = get_sports(c)
+        for s in sport:
+            if s == m[0]:
+                result.append(get_school(c))
+
+    return result
